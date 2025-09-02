f0c6e5af32f55e301a835119c7e5eee9ed0efa92
Gnana prakasam <gnanaprakasama30@gmail.com>
2025-09-02 09:36:07 +0530

    Configured Vite base path and updated deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/EyeCare-React-Frontend/',
   plugins: [
     react(),
     tailwindcss(), ]
